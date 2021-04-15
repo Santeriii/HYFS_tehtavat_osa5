3c5afd4ac88d8c6cca02b69838b8a29b49a4eacc
Santeri Einola <einola.santeri@gmail.com>
2021-04-15 18:02:11 +0300

    tehtävä 5.7 valmis

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [fullData, setFullData] = useState(false)
+
+  const handleFullData = () => {
+    setFullData(!fullData)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    !fullData ? (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={handleFullData}>view</button>
+      </div>
+    )
+    :
+    (
+      <div style={blogStyle}>
+        {blog.title}<button onClick={handleFullData}>hide</button><br/>
+        {blog.url}<br/>
+        likes {blog.likes}
+        <button>like</button><br/>
+        {blog.author}<br/>
+      </div>
+    )
+  )
+}
 
 export default Blog
