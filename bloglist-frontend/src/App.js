ece2658bb457642e43dce8d509d8abc836b22252
Santeri Einola <einola.santeri@gmail.com>
2021-04-15 14:33:30 +0300

    tehtävä 5.1 valmis

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +15,67 @@ const App = () => {
     )  
   }, [])
 
-  return (
-    <div>
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception)
+    }
+  }
+
+  const loginForm = () => {
+    return (
+      <>
+        <h2>log in to application</h2>
+          <form onSubmit={handleLogin}>
+            <div>
+              username
+                <input
+                type="text"
+                value={username}
+                name="Username"
+                onChange={({ target }) => setUsername(target.value)}
+                />
+            </div>
+            <div>
+              password
+                <input
+                type="password"
+                value={password}
+                name="Password"
+                onChange={({ target }) => setPassword(target.value)}
+                />
+            </div>
+            <button type="submit">login</button>
+          </form>
+        </>
+    )
+  }
+
+  const blogsView = () => {
+    return (
+    <>
       <h2>blogs</h2>
+      {user.name} logged in
+      <br/><br/>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </>
+    )
+  }
+
+  return (
+    <div>
+      {user === null && loginForm()}
+      {user !== null && blogsView()}
     </div>
   )
 }
