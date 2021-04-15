68b7ed6d4984625be7eacb609894aff34af826b0
Santeri Einola <einola.santeri@gmail.com>
2021-04-15 15:38:59 +0300

    tehtävä 5.3 valmis

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -8,11 +8,22 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
   }, [])
 
   const handleLogin = async (event) => {
@@ -22,9 +33,32 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
+    } catch (exception) {
+      console.log(exception)
+    }
+  }
+
+  const handleBlogSubmit = async (event) => {
+    event.preventDefault()
+
+    try {
+      const blog = {
+        title: title,
+        author: author,
+        url: url
+      }
+
+      const createdBlog = await blogService.create(blog)
+      setBlogs(blogs.concat(createdBlog))
     } catch (exception) {
       console.log(exception)
     }
@@ -59,16 +93,55 @@ const App = () => {
     )
   }
 
+  const blogForm = () => {
+    return (
+      <form onSubmit={handleBlogSubmit}>
+        <h2>create new</h2>
+        <br/>
+        <div>
+          title:
+          <input
+          type="text"
+          value={title}
+          name="Title"
+          onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author:
+          <input
+          type="text"
+          value={author}
+          name="Author"
+          onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url:
+          <input
+          type="text"
+          value={url}
+          name="Url"
+          onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    )
+  }
+
   const blogsView = () => {
     return (
-    <>
-      <h2>blogs</h2>
-      {user.name} logged in
-      <br/><br/>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </>
+      <>
+        <h2>blogs</h2>
+        {user.name} logged in
+        <button onClick={() => window.localStorage.removeItem('loggedBlogappUser')}>logout</button><br/>
+        {blogForm()}
+        <br/><br/>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </>
     )
   }
 
