79fb7ae36746590c8a8635a81b5ec5017b989972
Santeri Einola <einola.santeri@gmail.com>
2021-04-16 22:12:19 +0300

    tehtävä 5.14 valmis

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('initially renders thet title and the author of the blog, but not the likes nor the url', () => {
@@ -32,6 +32,42 @@ test('initially renders thet title and the author of the blog, but not the likes
     )
 
     expect(component.container).not.toHaveTextContent(
+        2
+    )
+})
+
+test('after clicking the "view"-button of the blog the data of the likes and the url are shown', () => {
+    const blog = {
+        title: 'Testiblogi',
+        author: 'Testaaja',
+        url: 'testiblogi.com',
+        likes: 2
+    }
+
+    const user = {
+        username: 'peke'
+    }
+
+    const component = render(
+        <Blog blog={blog} user={user} />
+    )
+
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    expect(component.container).toHaveTextContent(
+        'Testiblogi'
+    )
+
+    expect(component.container).toHaveTextContent(
+        'Testaaja'
+    )
+
+    expect(component.container).toHaveTextContent(
+        'testiblogi.com'
+    )
+    
+    expect(component.container).toHaveTextContent(
         2
     )
 })
